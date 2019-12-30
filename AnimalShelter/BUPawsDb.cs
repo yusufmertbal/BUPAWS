d65cc2b289e0c53f2c534e222271db2809bd084f
yusufmertbal <41000469+yusufmertbal@users.noreply.github.com>
2019-12-30 20:01:10 +0300

    First Commit

---
+++ AnimalShelter/BUPawsDb.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnimalShelter
+{
+   public class BUPawsDb : DbContext
+    {
+        string connectionString = @"Server=.\SQLEXPRESS;Database=BUPawsDb;Trusted_Connection=True;";
+        public DbSet<PawUser> PawUsers { get; set; }
+        public BUPawsDb() : base()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+    }
+}
---
+++ AnimalShelter/PawUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace AnimalShelter
+{
+    public class PawUser
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        public string Surname { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        public string UserName { get; set; }
+        [Required]
+        [StringLength(256)]
+        public string Password { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
